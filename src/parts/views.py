8767697618fc51a9d34bd6e1d670165055d7d7d2
Alex Baikov <baikov52@gmail.com>
2021-04-02 17:38:11 +0300

    add PartViewset

--- src/parts/views.py
+++ src/parts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.exceptions import NotFound, NotAcceptable
+from parts.serializers import PartSerializer
+from .models import Part
 
-# Create your views here.
+
+class SmallPagesPagination(PageNumberPagination):
+    page_size = 40
+
+
+class PartViewset(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PartSerializer
+    pagination_class = SmallPagesPagination
+
+    def get_queryset(self):
+        vin = self.request.query_params.get('vin', None)
+        if vin is not None:
+            parts = Part.objects.filter(vin=vin)
+            if parts:
+                return parts
+            else:
+                raise NotFound()
+        else:
+            raise NotAcceptable()
+        # return parts
