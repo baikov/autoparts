48a14be83bfc57cfe68ee5578523d2d37df8fac1
Alex Baikov <baikov52@gmail.com>
2021-04-02 21:33:44 +0300

    add parts url

---
+++ src/parts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('parts/', views.PartViewset.as_view()),
+    # path('import/', views.upload_csv_from_form, name='csv_import'),
+]
