bb4987753085700c1c0f7ced0c78796167feb764
Alex Baikov <baikov52@gmail.com>
2021-04-02 17:13:59 +0300

    add Part model

--- src/parts/models.py
+++ src/parts/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+
+class Part(models.Model):
+    vin = models.CharField(max_length=20)
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=300)
+
+    def __str__(self) -> str:
+        return self.code
