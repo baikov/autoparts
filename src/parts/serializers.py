0efcf96d7752200a9603a78993c077e381caab21
Alex Baikov <baikov52@gmail.com>
2021-04-02 17:17:03 +0300

    add PartSerializer

---
+++ src/parts/serializers.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+
+from .models import Part
+
+
+class PartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Part
+        fields = ['id', 'vin', 'code', 'name']
